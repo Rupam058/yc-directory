1c509e96e6fe839b79c70e2a6c5199d0a8c74266
Rupam058 <rupam.irp@gmail.com>
2024-11-18 11:09:34 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container pattern">
+        <h1 className="heading">
+          Pitch Your startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+}
