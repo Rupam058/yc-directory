1525aa0d39304c7cb46a0e817f2fbf695aeb0c36
Rupam058 <rupam.irp@gmail.com>
2024-11-30 21:43:49 +0530

    Parallel Loading

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -21,13 +21,18 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  // const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
-  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-    slug: "editor-picks",
-  });
+  // const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+  //   slug: "editor-picks",
+  // });
 
   if (!post) return notFound();
 
